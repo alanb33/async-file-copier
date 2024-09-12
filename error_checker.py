25f950c836f94c882aa84e57414f5c940981ec2e
alanb33 <ab33@protonmail.com>
2024-09-12 14:25:57 -0400

    Prevent reserved directory usage

--- error_checker.py
+++ error_checker.py
@@ -1,12 +1,13 @@
-import pathlib
+from pathlib import Path
 import sys
 
 def _check_for_errors(origin, destination):
     _check_for_source_match(origin, destination)
+    _check_for_reserved_directories(origin, destination)
     _check_for_empty_directory(destination)
 
 def _check_for_empty_directory(destination):
-    path = pathlib.Path(destination)
+    path = Path(destination)
     obj_count = 0
     for _ in path.iterdir():
         obj_count += 1
@@ -22,13 +23,19 @@ def _check_for_empty_directory(destination):
             except KeyboardInterrupt:
                 sys.exit("\nAborting.")
 
+def _check_for_reserved_directories(*args):
+
+    for directory in args:
+        if Path(directory).is_dir() and Path(directory).is_reserved():
+            sys.exit(f"Unable to operate on reserved directory [{Path(directory).absolute()}].")
+
 def _check_for_source_match(origin, destination):
     if origin == destination:
         sys.exit("Origin and destination cannot be the same directory.")
 
 def _validate_sources(*args):
     for directory in args:
-        dir = pathlib.Path(directory)
+        dir = Path(directory)
         if dir.exists():
             if not dir.is_dir():
                 sys.exit(f"Did not receive a valid directory path: {dir}")
